39ffc88050ab88c055900597d52821fd5b246d32
vroudge <v.roudge@gmail.com>
2017-09-22 16:47:35 +0200

    Fix sound playing on fail

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const path = require('path');
 const isWindows = process.platform === 'win32';
 
 class jestFuck {
-    constructor() {
+    constructor () {
         this.fuck = path.join(path.resolve(__dirname), 'misc/fuck.wav');
     }
 
-    playSound(sound) {
-        soundPlayer.play(sound, function(err) {
+    playSound (sound) {
+        soundPlayer.play(sound, function (err) {
             if (err && isWindows) {
                 console.log('You are running windows. You need \'mplayer\' installed for jest-fuck to work properly.');
                 console.log('See https://www.npmjs.com/package/play-sound');
@@ -18,10 +18,8 @@ class jestFuck {
         });
     }
 
-    onRunComplete(contexts, results) {
-        if (!results.numTotalTests || !results.numFailedTests) {
-            this.playSound(this.fuck);
-        }
+    onRunComplete (contexts, results) {
+        results.numFailedTests > 0 && this.playSound(this.fuck);
     }
 }
 
